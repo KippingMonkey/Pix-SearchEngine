9faade372c591b2c68c477724336ff15df53ffbf
Jonas <jonas@kippingmonkey.se>
2021-09-23 23:50:20 +0200

    Add colored highlights for choices on mouseover. Change color of brush to chosen color

--- functions.js
+++ functions.js
@@ -1,3 +1,4 @@
+function showDropdown(){
 let dropdownMenu = document.getElementById("dropdown-menu");
 let dropdownbtn = document.getElementById("paintbrush");
 
@@ -7,11 +8,63 @@ dropdownbtn.addEventListener("mouseover",function(){
 window.onclick = function(){
     dropdownMenu.style.display = "none";
 }
-// dropdownbtn.addEventListener("mouseout",function(){
-//     setTimeout(function(){
-// dropdownMenu.style.display = "none";
-// }, 1000); 
-// })
+}
+showDropdown();
+
+
+function colorBrush(){
+let radiobtns = Array.from(document.querySelectorAll('[type="radio"]'));
+let htmlcolors = ["#bcbcbc","gray","#F8F8F8","red","orange","green","turquoise","blue","purple","pink","white","gray","black","#964B00" ]
+let brush = document.getElementById("paintbrush");
+
+for (let i = 0; i < radiobtns.length; i++) {
+    let btn = document.querySelector(`[value="${i}"]`);
+    btn.addEventListener("click",function(){
+    // alert(`Color: ${btn.id} got clicked`);
+        if (i === 10) {
+        brush.style.backgroundColor = "gray"
+        }
+        else{
+        brush.style.backgroundColor = "white"
+    }
+brush.style.color = htmlcolors[i];
+});
+}
+}
+colorBrush();
+
+function highlightChoice(){
+    let htmlcolors = ["#bcbcbc","gray","#F8F8F8","red","orange","green","turquoise","blue","purple","pink","white","gray","black","#964B00" ]
+    var labels = Array.from(document.querySelectorAll(".c-label"));
+for (let i = 0; i < labels.length; i++) {
+    labels[i].addEventListener("mouseover",function(){
+        // alert(`label ${i}`)
+    let label = this;
+    label.style.backgroundColor = htmlcolors[i];
+    if (i === 2 || i === 10) {
+        label.style.color = "black";
+        label.style.outline ="1px solid gray";
+    }
+    else{label.style.color="white";}
+    });
+}
+}
+highlightChoice();
+
+function resetChoice(){
+    var labels = Array.from(document.querySelectorAll(".c-label"));
+for (let i = 0; i < labels.length; i++) {
+    labels[i].addEventListener("mouseout",function(){
+        // alert(`label ${i}`)
+    let label = this;
+    label.style.backgroundColor = "white";
+    label.style.color = "black";
+    label.style.outline = 0;
+    });
+}
+}
+resetChoice();
+
 
 
 
