e16964b4bd123719a179becd1225c25e31fa97d7
Johan H <72360690+EagleJohan@users.noreply.github.com>
2021-09-27 11:53:54 +0200

    Added navigation btns

--- functions.js
+++ functions.js
@@ -2,10 +2,9 @@ const form = document.getElementById("toolbar-form");
 const searchContainer = document.getElementById("search-container");
 const colorSelect = document.getElementById("color-select");
 const gallery = document.getElementById("gallery");
+let pageNumber = 1;
 
-function getSearchResults() {
-  form.onsubmit = async (event) => {
-    event.preventDefault();
+async function getSearchResults() {
     gallery.replaceChildren();
     // document.getElementById("gallery").style.display = "flex";
     if (!searchContainer.classList.contains("search-active")) {
@@ -13,35 +12,38 @@ function getSearchResults() {
     }
 
     //let searchFor = form.elements.namedItem("searchfield"); (=alternative way of typing)
-    const searchFor = form.elements.searchfield.value;
-    const color = colorSelect.value;
-    const apiKey = "23538954-f5928fdd584dadd6f32fceceb";
+    const searchValue = form.elements.searchfield.value;
+    // const color = colorSelect.value;
+    // const apiKey = "23538954-f5928fdd584dadd6f32fceceb";
 
-    const searchString = new URLSearchParams({
-      key: apiKey,
-      q: searchFor,
-      colors: color,
-      per_page: 10
-    });
-    const response = await fetch("https://pixabay.com/api/?" + searchString);
+    // const searchString = new URLSearchParams({
+    //   key: apiKey,
+    //   q: searchFor,
+    //   colors: color,
+    //   per_page: 10,
+    //   page: 1
+    // });
+    // const response = await fetch("https://pixabay.com/api/?" + searchString);
 
-    if (!response.ok) {
-      const message = `An error has occured: ${response.status}`;
-      throw new Error(message);
-    }
-    const searchResult = await response.json();
+    // if (!response.ok) {
+    //   const message = `An error has occured: ${response.status}`;
+    //   throw new Error(message);
+    // }
+    // const searchResult = await response.json();
+    const searchResult = await fetchData(pageNumber);
+    const maxPages = Math.ceil(searchResult.totalHits / 10);
     const header = document.createElement("h2");
-    if(searchFor === ""){
-        header.textContent = "Search results:"
+    if (searchValue === "") {
+      header.textContent = "Search results:";
     } else if (searchResult.totalHits > 0) {
-        header.textContent = `Search results for ${searchFor.toLowerCase()}`;
+      header.textContent = `Search results for ${searchValue.toLowerCase()}:`;
     } else {
-        header.textContent = "No search results. Try another keyword";
+      header.textContent = "No search results. Try another keyword";
     }
     gallery.appendChild(header);
 
-    searchResult.hits.forEach(data => {
-        gallery.appendChild(createFigure(data));
+    searchResult.hits.forEach((data) => {
+      gallery.appendChild(createFigure(data));
     });
     // let figures = document.querySelectorAll("figure");
     // let images = document.querySelectorAll(".search-result");
@@ -55,30 +57,96 @@ function getSearchResults() {
     //   figCap1[i].innerHTML = `#: ${searchResult.hits[i].tags}`;
     //   figCap2[i].innerHTML = `User: ${searchResult.hits[i].user}`;
     // }
+    gallery.appendChild(createButtons(maxPages));
   };
+
+
+function createButtons(maxPages) {
+  const btnContainer = document.createElement("div");
+  btnContainer.classList.add("btn-container");
+
+  const backward = document.createElement("button");
+  const iconBack = document.createElement("i");
+  iconBack.classList.add("fas", "fa-backward", "fa-2x");
+  backward.appendChild(iconBack);
+  if (pageNumber === 1) {
+    backward.disabled = true;
+  } else {
+    backward.disabled = false;
+  }
+
+  backward.addEventListener("click", () => {
+      pageNumber--;
+      getSearchResults();
+    });
+    
+    const forward = document.createElement("button");
+    const iconForward = document.createElement("i");
+    iconForward.classList.add("fas", "fa-forward", "fa-2x");
+    forward.appendChild(iconForward);
+    if (pageNumber === maxPages) {
+        forward.disabled = true;
+    } else {
+        forward.disabled = false;
+    }
+    
+    forward.addEventListener("click", () => {
+      pageNumber++;
+      getSearchResults();
+  })
+
+  const currentPage = document.createElement("h3");
+  currentPage.textContent = pageNumber;
+
+  btnContainer.appendChild(backward);
+  btnContainer.appendChild(currentPage);
+  btnContainer.appendChild(forward);
+
+  return btnContainer;
+}
+
+async function fetchData(page) {
+  const searchFor = form.elements.searchfield.value;
+  const color = colorSelect.value;
+  const apiKey = "23538954-f5928fdd584dadd6f32fceceb";
+
+  const searchString = new URLSearchParams({
+    key: apiKey,
+    q: searchFor,
+    colors: color,
+    per_page: 10,
+    page: page,
+  });
+  const response = await fetch("https://pixabay.com/api/?" + searchString);
+
+  if (!response.ok) {
+    const message = `An error has occured: ${response.status}`;
+    throw new Error(message);
+  }
+  return await response.json();
 }
 
 function createFigure(data) {
-    const figure = document.createElement("figure");
-    const hyperlink = document.createElement("a");
-    const image = document.createElement("img");
-    const tags = document.createElement("figcaption");
-    const photographer = document.createElement("figcaption");
+  const figure = document.createElement("figure");
+  const hyperlink = document.createElement("a");
+  const image = document.createElement("img");
+  const tags = document.createElement("figcaption");
+  const photographer = document.createElement("figcaption");
 
-    figure.style.display = "inline-block";
-    image.src = data.webformatURL;
-    image.alt = data.tags;
-    tags.textContent = `#: ${data.tags}`;
-    photographer.textContent = `User: ${data.user}`;
-    hyperlink.href = data.webformatURL;
-    hyperlink.target = "_blank";
+  figure.style.display = "inline-block";
+  image.src = data.webformatURL;
+  image.alt = data.tags;
+  tags.textContent = `#: ${data.tags}`;
+  photographer.textContent = `User: ${data.user}`;
+  hyperlink.href = data.webformatURL;
+  hyperlink.target = "_blank";
 
-    image.appendChild(hyperlink);
-    figure.appendChild(image);
-    figure.appendChild(tags);
-    figure.appendChild(photographer);
+  image.appendChild(hyperlink);
+  figure.appendChild(image);
+  figure.appendChild(tags);
+  figure.appendChild(photographer);
 
-    return figure;
+  return figure;
 }
 
 function createGallery(node) {
@@ -87,7 +155,13 @@ function createGallery(node) {
 }
 
 // createGallery(document.getElementById("gallery"));
-getSearchResults();
+// getSearchResults();
+
+form.onsubmit = async (event) => {
+    pageNumber = 1;
+    event.preventDefault();
+    getSearchResults();
+}
 
 // function showDropdown() {
 //   let dropdownMenu = document.getElementById("dropdown-menu");
